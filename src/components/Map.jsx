d48ccb88c4315169af9c06e93757b455bcbace51
veddster <matthewvedder@gmail.com>
2017-01-26 21:08:53 -0500

    refactor url prop in Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -27,13 +27,15 @@ class MyMap extends Component {
   }
 
   render() {
+    // This needs to be hidden
     const accessToken = 'pk.eyJ1IjoidmVkZHN0ZXIiLCJhIjoiY2lyNzdlanUzMDBza2djbTM1Z2hlYTdnNSJ9.FpydM0KRrrunXzaHJYCmrA'
+    const url = 'https://api.mapbox.com/styles/v1/veddster/cire76fb60003g9njs0l9hf9k/tiles/256/'
 
     return (
       <div>
         <Map onClick={this.onMapClick} style={styles.mapStyle} center={position} zoom={11}>
           <TileLayer
-            url={`https://api.mapbox.com/styles/v1/veddster/cire76fb60003g9njs0l9hf9k/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`}
+            url={`${url}{z}/{x}/{y}?access_token=${accessToken}`}
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           <Marker position={position}>
