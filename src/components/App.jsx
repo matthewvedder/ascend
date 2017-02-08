ee69becb6aee6a1f000d4fa1ed3103332bfd4318
veddster <matthewvedder@gmail.com>
2017-02-08 17:10:55 -0500

    refactor AuthSagas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,6 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.props.auth)
     return (
       <div className="App">
         <Button
--- src/sagas/AuthSagas.js
+++ src/sagas/AuthSagas.js
@@ -3,19 +3,23 @@ import { takeLatest, put } from 'redux-saga/effects'
 import { GOOGLE_AUTH, HANDLE_AUTH_DATA } from '../actions/types'
 
 function* googleAuth() {
-// THIS IS REALLY UGLY
   const provider = new firebase.auth.GoogleAuthProvider()
-  let token = ''
-  let user = {}
 
-  yield firebase.auth().signInWithPopup(provider).then((result) => {
-    token = result.credential.accessToken
-    user = result.user.providerData[0]
-  }).catch((error) => {
-    console.warn(error)
-  })
+  const auth = yield firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      return {
+        token: result.credential.accessToken,
+        user: result.user.providerData[0]
+      }
+    }).catch((error) => {
+      console.warn(error)
+    })
 
-  yield put({ type: HANDLE_AUTH_DATA, token, user })
+  const { user, token } = auth
+
+  yield put({ type: HANDLE_AUTH_DATA, user, token, provider })
 }
 
 export function* watchAuth() {
