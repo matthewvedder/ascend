d2d1bfcb6b651b9584d9a9688fdcc36e4a470c96
veddster <matthewvedder@gmail.com>
2017-02-09 16:25:20 -0500

    remove dispatched action from AuthSagas

--- src/sagas/AuthSagas.js
+++ src/sagas/AuthSagas.js
@@ -1,25 +1,15 @@
 import firebase from 'firebase'
-import { takeLatest, put } from 'redux-saga/effects'
-import { GOOGLE_AUTH, HANDLE_AUTH_DATA } from '../actions/types'
+import { takeLatest } from 'redux-saga/effects'
+import { GOOGLE_AUTH } from '../actions/types'
 
 function* googleAuth() {
   const provider = new firebase.auth.GoogleAuthProvider()
 
-  const auth = yield firebase
+  yield firebase
     .auth()
     .signInWithPopup(provider)
-    .then((result) => {
-      return {
-        token: result.credential.accessToken,
-        user: result.user.providerData[0]
-      }
-    }).catch((error) => {
-      console.warn(error)
-    })
-
-  const { user, token } = auth
-
-  yield put({ type: HANDLE_AUTH_DATA, user, token, provider })
+    .then(result => ({ token: result.credential.accessToken }))
+    .catch(error => ({ error }))
 }
 
 export function* watchAuth() {
